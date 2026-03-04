216ed3448d17d656272a467ce92520fa234c1543
KIMDONGWOOK12 <rlaehddnr8838@gmail.com>
2026-03-05 02:08:48 +0900

    login,Signup 작성 및 메인 건들였음

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from fastapi import FastAPI
 from app.database import engine, Base
-from app.models import user  # 방금 만든 유저 모델 불러오기
+from app.models import user  #  중요
+from app.api import logins, signs_up 
 
-# DB 테이블 생성 (이미 있으면 생성 안 함)
+# 이 코드가 실행되는 순간 orbit.db 파일에 'users' 테이블이 생성될거래
 Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Orbit SNS System")
+
+# 이렇게 연결하면 브라우저에서 /signup, /login 주소로 접근할 수 있음
+app.include_router(signs_up.router, prefix="/signup", tags=["Registration"])
+app.include_router(logins.router, prefix="/login", tags=["Authentication"])
+
+@app.get("/")
+def root():
+    return {"message": "Orbit SNS 서버가 정상 작동 중입니다!"}
